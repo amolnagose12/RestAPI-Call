c050ff6c57e9212ea127c73232714af2e29b2a18
amolnagose12 <amolnagose12@gmail.com>
2021-12-17 23:47:40 +0530

    Post request method Pass firdt name and last name in the body

---
+++ src/main/java/com/demo/example/model/User.java
@@ -0,0 +1,26 @@
+package com.demo.example.model;
+
+public class User {
+	private String firstName;
+	private String lastName;
+
+	/**
+	 * setter and getter methods
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+}
--- src/main/java/com/example/demo/controller/HelloRestController.java
+++ src/main/java/com/example/demo/controller/HelloRestController.java
@@ -2,10 +2,14 @@ package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.demo.example.model.User;
 
 /**
  * 
@@ -39,6 +43,15 @@ public class HelloRestController {
 	@GetMapping("/param/{name}")
 	public String sayHelloPara(@PathVariable String name) {
 		return "Hello " + name + " From Bridgelabz";
+	}
+	/**
+	 * @PostMapping:handle the HTTP POST requests matched with given URL expression
+	 * @RequestBody:
+	 * @return
+	 */
+	@PostMapping("/post")
+	public String sayHello(@RequestBody User user) {
+		return "Hello " + user.getFirstName() + " " + user.getLastName() + " From Bridgelabz";
 	}
 
 }
